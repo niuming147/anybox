d8fb5109d0f35981a37c923dd32c2a61f0ade447
niuming147 <niuming147@gmail.com>
2016-10-12 11:55:15 +0800

    add nmxyz

---
+++ nmxyz/src/main/java/com/nm147/nmtest2/EncyptUtil.java
@@ -0,0 +1,80 @@
+package com.nm147.nmtest2;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+@SuppressWarnings("restriction")
+public class EncyptUtil {
+	// 加密
+	public static String base64Encode(String str) {
+		byte[] b = null;
+		String s = null;
+		try {
+			b = str.getBytes("utf-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		if (b != null) {
+			s = new BASE64Encoder().encode(b);
+		}
+		return s;
+	}
+
+	// 解密
+	public static String base64Decode(String s) {
+		byte[] b = null;
+		String result = null;
+		if (s != null) {
+			BASE64Decoder decoder = new BASE64Decoder();
+			try {
+				b = decoder.decodeBuffer(s);
+				result = new String(b, "utf-8");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public static String md5Encypt(String plainText) {
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+		try {
+			byte[] btInput = plainText.getBytes();
+			// 获得MD5摘要算法的 MessageDigest 对象
+			MessageDigest mdInst = MessageDigest.getInstance("MD5");
+			// 使用指定的字节更新摘要
+			mdInst.update(btInput);
+			// 获得密文
+			byte[] md = mdInst.digest();
+			// 把密文转换成十六进制的字符串形式
+			int j = md.length;
+			char str[] = new char[j * 2];
+			int k = 0;
+			for (int i = 0; i < j; i++) {
+				byte byte0 = md[i];
+				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+				str[k++] = hexDigits[byte0 & 0xf];
+			}
+			return new String(str);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static String reverse(String str) {
+		return new StringBuilder(str).reverse().toString();
+	}
+
+	public static String upCase(String str) {
+		return str.toUpperCase();
+	}
+
+	public static String lowCase(String str) {
+		return str.toLowerCase();
+	}
+
+}
